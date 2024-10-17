c244d5bbd8c47ea27575d604d8981c0453eb610b
Michael Benardis <michael.benardis@gmail.com>
2024-10-17 11:06:16 +0300

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -9,8 +9,8 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now)
-    publish_date = models.DateTimeField(blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.publish_date = timezone.now()
